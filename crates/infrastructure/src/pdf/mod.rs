35486c4ac99d3a96c11cddd9d1c2c941cf8673c8
tarekul42 <tarekulrifat205@gmail.com>
2026-07-13 04:22:06 +0600

    Update printpdf dependency to 0.10 and migrate PDF generation code

--- crates/infrastructure/src/pdf/mod.rs
+++ crates/infrastructure/src/pdf/mod.rs
@@ -22,67 +22,125 @@ pub struct InvoiceData {
 ///
 /// Returns the raw PDF bytes.
 pub fn generate_invoice(data: &InvoiceData) -> Result<Vec<u8>, PdfError> {
-    let (doc, page1, layer1) = PdfDocument::new(
-        format!("Invoice {}", data.transaction_id),
+    let mut doc = PdfDocument::new(&format!("Invoice {}", data.transaction_id));
+
+    let helv = PdfFontHandle::Builtin(BuiltinFont::Helvetica);
+    let helv_bold = PdfFontHandle::Builtin(BuiltinFont::HelveticaBold);
+    let mut warnings = Vec::new();
+
+    let page = PdfPage::new(
         Mm(210.0),
         Mm(297.0),
-        "Layer 1",
+        vec![
+            Op::StartTextSection,
+            Op::SetFont {
+                font: helv_bold.clone(),
+                size: Pt(24.0),
+            },
+            Op::SetTextCursor {
+                pos: Point::new(Mm(20.0), Mm(270.0)),
+            },
+            Op::ShowText {
+                items: vec!["INVOICE".into()],
+            },
+            Op::SetFont {
+                font: helv.clone(),
+                size: Pt(12.0),
+            },
+            Op::SetTextCursor {
+                pos: Point::new(Mm(20.0), Mm(255.0)),
+            },
+            Op::ShowText {
+                items: vec![format!("Invoice #: {}", data.transaction_id).into()],
+            },
+            Op::SetTextCursor {
+                pos: Point::new(Mm(20.0), Mm(250.0)),
+            },
+            Op::ShowText {
+                items: vec![TextItem::Text("_".repeat(70))],
+            },
+            Op::SetFont {
+                font: helv_bold.clone(),
+                size: Pt(14.0),
+            },
+            Op::SetTextCursor {
+                pos: Point::new(Mm(20.0), Mm(240.0)),
+            },
+            Op::ShowText {
+                items: vec!["Bill To:".into()],
+            },
+            Op::SetFont {
+                font: helv.clone(),
+                size: Pt(12.0),
+            },
+            Op::SetTextCursor {
+                pos: Point::new(Mm(20.0), Mm(230.0)),
+            },
+            Op::ShowText {
+                items: vec![data.user_name.as_str().into()],
+            },
+            Op::SetTextCursor {
+                pos: Point::new(Mm(20.0), Mm(220.0)),
+            },
+            Op::ShowText {
+                items: vec![data.user_email.as_str().into()],
+            },
+            Op::SetFont {
+                font: helv_bold.clone(),
+                size: Pt(14.0),
+            },
+            Op::SetTextCursor {
+                pos: Point::new(Mm(20.0), Mm(205.0)),
+            },
+            Op::ShowText {
+                items: vec!["Workshop:".into()],
+            },
+            Op::SetFont {
+                font: helv.clone(),
+                size: Pt(12.0),
+            },
+            Op::SetTextCursor {
+                pos: Point::new(Mm(20.0), Mm(195.0)),
+            },
+            Op::ShowText {
+                items: vec![data.workshop_title.as_str().into()],
+            },
+            Op::SetFont {
+                font: helv_bold,
+                size: Pt(14.0),
+            },
+            Op::SetTextCursor {
+                pos: Point::new(Mm(20.0), Mm(175.0)),
+            },
+            Op::ShowText {
+                items: vec!["Amount Paid:".into()],
+            },
+            Op::SetFont {
+                font: helv.clone(),
+                size: Pt(12.0),
+            },
+            Op::SetTextCursor {
+                pos: Point::new(Mm(20.0), Mm(165.0)),
+            },
+            Op::ShowText {
+                items: vec![format!("BDT {:.2}", data.amount.cents() as f64 / 100.0).into()],
+            },
+            Op::SetFont {
+                font: helv.clone(),
+                size: Pt(10.0),
+            },
+            Op::SetTextCursor {
+                pos: Point::new(Mm(20.0), Mm(30.0)),
+            },
+            Op::ShowText {
+                items: vec!["Thank you for your purchase!".into()],
+            },
+            Op::EndTextSection,
+        ],
     );
 
-    let font = doc
-        .add_builtin_font(BuiltinFont::Helvetica)
-        .map_err(|e| PdfError::GenerationFailed(e.to_string()))?;
-    let font_bold = doc
-        .add_builtin_font(BuiltinFont::HelveticaBold)
-        .map_err(|e| PdfError::GenerationFailed(e.to_string()))?;
-
-    let current_layer = doc.get_page(page1).get_layer(layer1);
-
-    // Title
-    current_layer.use_text("INVOICE", 24.0, Mm(20.0), Mm(270.0), &font_bold);
-
-    // Transaction ID
-    current_layer.use_text(
-        format!("Invoice #: {}", data.transaction_id),
-        12.0,
-        Mm(20.0),
-        Mm(255.0),
-        &font,
-    );
-
-    // Separator
-    current_layer.use_text("_".repeat(70), 10.0, Mm(20.0), Mm(250.0), &font);
-
-    // Customer info
-    current_layer.use_text("Bill To:", 14.0, Mm(20.0), Mm(240.0), &font_bold);
-    current_layer.use_text(&data.user_name, 12.0, Mm(20.0), Mm(230.0), &font);
-    current_layer.use_text(&data.user_email, 12.0, Mm(20.0), Mm(220.0), &font);
-
-    // Workshop info
-    current_layer.use_text("Workshop:", 14.0, Mm(20.0), Mm(205.0), &font_bold);
-    current_layer.use_text(&data.workshop_title, 12.0, Mm(20.0), Mm(195.0), &font);
-
-    // Amount
-    current_layer.use_text("Amount Paid:", 14.0, Mm(20.0), Mm(175.0), &font_bold);
-    current_layer.use_text(
-        format!("BDT {:.2}", data.amount.cents() as f64 / 100.0),
-        12.0,
-        Mm(20.0),
-        Mm(165.0),
-        &font,
-    );
-
-    // Footer
-    current_layer.use_text(
-        "Thank you for your purchase!",
-        10.0,
-        Mm(20.0),
-        Mm(30.0),
-        &font,
-    );
-
-    doc.save_to_bytes()
-        .map_err(|e| PdfError::GenerationFailed(e.to_string()))
+    doc.with_pages(vec![page]);
+    Ok(doc.save(&PdfSaveOptions::default(), &mut warnings))
 }
 
 /// Errors from PDF generation.
